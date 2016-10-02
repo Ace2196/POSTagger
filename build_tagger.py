ac14b6340ce21517f4fa6faed50ee76335fd25fc
Ace2196 <abdulla.contractor@gmail.com>
2016-10-02 19:30:41 +0800

    update the use part of the dev set for testing

--- build_tagger.py
+++ build_tagger.py
@@ -21,6 +21,8 @@ with open(FILE_TRAIN, 'r') as f:
         sents.append(s.split())
 with open(FILE_DEVT, 'r') as f:
     for s in f:
+        if len(devt_sents) == 100:
+            devt_sents = []
         devt_sents.append(s.split())
 
 vocab = set()
